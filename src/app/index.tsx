d5010d7ca79f1c54e8a67f9fdd3ef551f48172fe
gpmedical <dr.gerardo.perrotta@gmail.com>
2026-06-02 20:54:45 +0200

    Update lightningcss version to 1.30.1 in package.json, pnpm-lock.yaml, and pnpm-workspace.yaml; refactor Index component in index.tsx for improved styling.

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,16 +2,9 @@ import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
-    <View style={styles.container}>
-      <Text>HEADLY</Text>
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-xl text-indigo-600 text-center mt-90 font-bold">HEADLY</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
